2d2c64f510c583f0f595035fcd4a5cd1b1f12036
Jannis Aziz Khammas <aziz_khammas@yahoo.de>
2022-01-14 10:05:20 +0100

    Updated build-frontend.sh

--- build-frontend.sh
+++ build-frontend.sh
@@ -4,4 +4,5 @@ cd ../frontend
 npm install && npm run build
 cd ..
 
-cp -r ./frontend/build/* ./backend/target/classes/static
+#cp -r ./frontend/build/* ./backend/target/classes/static
+cp -r ./frontend/build/* ./backend/src/main/resources/static
